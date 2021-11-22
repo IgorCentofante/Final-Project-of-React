a563da369a2034703ed2d887c3402497c4c0cb09
Bruno <brunodiedrich@gmail.com>
2021-11-22 19:55:58 -0300

    modificando CSS do componente sobreNos

--- src/components/SobreNos.js
+++ src/components/SobreNos.js
@@ -5,17 +5,17 @@ export default function SobreNos(){
 
     return(
 
-        <section className="sobre-nos">
-        <div className="sobre-nos-painel">
-            <h1 className= "sobre-nos-titulo" id="sobre-nos">SOBRE NÓS</h1>
-            <div className="sobre-nos-bg">
-                <h2 id = "editarSobre" className= "sobre-nos-sub">
+        <section className="sobre_nos">
+        <div className="sobre_nos_painel">
+            <h1 className= "sobre_nos-titulo" id="sobre_nos">SOBRE NÓS</h1>
+            <div className="sobre_nos_bg">
+                <h2 id = "editarSobre" className= "sobre_nos_sub">
                     Construído em  1947, o Hospital Panaceia tem como objetivo o conforto e segurança para seus pacientes.  Há 74 anos mantemos nosso lema sobre que a transformação é algo natural para todos. Sempre com muito conforto recebemos diversos pacientes não só do estado, mas também do país inteiro. Os melhores profissionais do país estão aqui. Venha logo marcar sua consulta.
                 </h2>
             </div>
         </div>
 
-        <div className="sobre-nos-img">
+        <div className="sobre_nos_img">
             <img
             src={imgTeto}
             alt="recepção"/>
