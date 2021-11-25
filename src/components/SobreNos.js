bdeb8c06f38185d3b494f720372346f3f3b63ce3
Bruno <brunodiedrich@gmail.com>
2021-11-25 13:56:43 -0300

    Corrigindo titulo sobre-nos

--- src/components/SobreNos.js
+++ src/components/SobreNos.js
@@ -10,7 +10,7 @@ export default function SobreNos(){
 
         <section className="sobre_nos">
         <div className="sobre_nos_painel">
-            <h1 className= "sobre_nos-titulo" id="sobre_nos">SOBRE NÓS</h1>
+            <h1 className= "sobre_nos_titulo" id="sobre_nos">SOBRE NÓS</h1>
             <div className="sobre_nos_bg">
                 <h2 id = "editarSobre" className= "sobre_nos_sub">
                     Construído em  1947, o Hospital Panaceia tem como objetivo o conforto e segurança para seus pacientes.  Há 74 anos mantemos nosso lema sobre que a transformação é algo natural para todos. Sempre com muito conforto recebemos diversos pacientes não só do estado, mas também do país inteiro. Os melhores profissionais do país estão aqui. Venha logo marcar sua consulta.
